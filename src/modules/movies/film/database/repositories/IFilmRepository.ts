46757f006c237de5a5634dda30af324e8ae6de8b
Bueno Nepomuceno <buenonp@gmail.com>
2023-06-07 12:30:33 -0300

    updated for dtos IFilmRepository

--- src/modules/movies/film/database/repositories/IFilmRepository.ts
+++ src/modules/movies/film/database/repositories/IFilmRepository.ts
@@ -1,12 +1,9 @@
-import { Film } from "../entities/film.entity";
-
-import { CreateFilmDTO } from "../../dtos/CreateFilmDTO";
-import { UpdateFilmDTO } from "../../dtos/UpdateFilmDTO";
+import { FilmDTO, CreateFilmDTO, UpdateFilmDTO } from "../../dtos/FilmDTO";
 
 export interface IFilmRepository {
-    getFilms(): Promise<Film[]>;
-    getFilmById(id: number): Promise<Film>;
-    createFilm(film: CreateFilmDTO): Promise<Film>;
-    updateFilm(id: number, film: UpdateFilmDTO): Promise<Film>;
+    getFilms(): Promise<FilmDTO[]>;
+    getFilmById(id: number): Promise<FilmDTO>;
+    createFilm(film: CreateFilmDTO): Promise<FilmDTO>;
+    updateFilm(id: number, film: UpdateFilmDTO): Promise<FilmDTO>;
     deleteFilm(id: number): Promise<void>;
 }
