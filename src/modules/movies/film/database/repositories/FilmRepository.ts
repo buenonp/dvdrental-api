74660b49441f4db5065ac73afe3a78482eb12bfd
Bueno Nepomuceno <buenonp@gmail.com>
2023-06-08 17:46:27 -0300

    base repository

--- src/modules/movies/film/database/repositories/FilmRepository.ts
+++ src/modules/movies/film/database/repositories/FilmRepository.ts
@@ -1,42 +1,10 @@
-import { Repository } from 'typeorm';
+import { 
+    FilmDTO, 
+    CreateFilmDTO, 
+    UpdateFilmDTO 
+} from "../../dtos/FilmDTO";
 
-import { Film } from '../entities/film.entity';
-import { IFilmRepository } from './IFilmRepository';
+import { BaseRepository } from '@/modules/base/BaseRepository';
 
-import { CreateFilmDTO, UpdateFilmDTO } from '../../dtos/FilmDTO';
-
-import dataSource from '@config/database/typeorm/data-source';
-
-export class FilmRepository implements IFilmRepository {
-    private readonly typeormRepository: Repository<Film>;
-    
-    constructor() {
-        this.typeormRepository = dataSource.getRepository(Film);
-    }
-
-    async getFilms(): Promise<Film[]> {
-        return await this.typeormRepository.find();
-    }
-
-    async getFilmById(film_id: number): Promise<Film> {
-        return await this.typeormRepository.findOneBy({ film_id });
-    }
-
-    async createFilm(film: CreateFilmDTO): Promise<Film> {
-        const newFilm = this.typeormRepository.create(film);
-
-        return await this.typeormRepository.save(newFilm);
-    }
-
-    async updateFilm(id: number, film: UpdateFilmDTO): Promise<Film> {
-        await this.typeormRepository.update(id, film);
-
-        return await this.getFilmById(id);
-    }
-
-    async deleteFilm(id: number): Promise<void> {
-        const film = await this.getFilmById(id);
-
-        await this.typeormRepository.delete(id);
-    }
-}
+export interface FilmRepository 
+    extends BaseRepository<FilmDTO, CreateFilmDTO, UpdateFilmDTO> { }
--- src/modules/movies/film/database/repositories/IFilmRepository.ts
+++
@@ -1,9 +0,0 @@
-import { FilmDTO, CreateFilmDTO, UpdateFilmDTO } from "../../dtos/FilmDTO";
-
-export interface IFilmRepository {
-    getFilms(): Promise<FilmDTO[]>;
-    getFilmById(id: number): Promise<FilmDTO>;
-    createFilm(film: CreateFilmDTO): Promise<FilmDTO>;
-    updateFilm(id: number, film: UpdateFilmDTO): Promise<FilmDTO>;
-    deleteFilm(id: number): Promise<void>;
-}
