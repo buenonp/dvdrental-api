2a310dc8c7552c1af1aa0ac0cc21dd94ad690fa6
Bueno Nepomuceno <buenonp@gmail.com>
2023-06-08 17:46:52 -0300

    base service

---
+++ src/modules/base/BaseServiceImpl.ts
@@ -0,0 +1,32 @@
+import { BaseService } from './BaseService';
+import { BaseRepository } from '@/modules/base/BaseRepository';
+import { injectable, inject } from 'tsyringe';
+
+@injectable()
+export class BaseServiceImpl<T, U, V> implements BaseService<T, U, V> {
+    
+    constructor(
+        @inject('BaseRepository')
+        private baseRepository: BaseRepository<T, U, V>
+    ) {}
+
+    async getItems(): Promise<T[]> {
+        return this.baseRepository.getItems();
+    }
+
+    async getItemById(id: number): Promise<T> {
+        return this.baseRepository.getItemById(id);
+    }
+
+    async createItem(item: U): Promise<T> {
+        return this.baseRepository.createItem(item);
+    }
+
+    async updateItem(id: number, item: V): Promise<T> {
+        return this.baseRepository.updateItem(id, item);
+    }
+
+    async deleteItem(id: number): Promise<void> {
+        return this.baseRepository.deleteItem(id);
+    }
+}
--- src/modules/movies/film/business/services/FilmService.ts
+++ src/modules/movies/film/business/services/FilmService.ts
@@ -1,34 +1,10 @@
-import { IFilmRepository } from '../../database/repositories/IFilmRepository';
+import { BaseService } from '@/modules/base/BaseService';
 
-import { FilmDTO,  CreateFilmDTO, UpdateFilmDTO } from '../../dtos/FilmDTO';
+import { 
+    FilmDTO,  
+    CreateFilmDTO, 
+    UpdateFilmDTO 
+} from '../../dtos/FilmDTO';
 
-import { IFilmService } from './IFilmService';
-
-export class FilmService implements IFilmService {
-    private readonly filmRepository: IFilmRepository;
-
-    constructor(filmRepository: IFilmRepository) {
-        this.filmRepository = filmRepository;
-    }
-
-    async getFilms(): Promise<FilmDTO[]> {
-        return this.filmRepository.getFilms();
-    }
-
-    async getFilmById(film_id: number): Promise<FilmDTO> {
-        return this.filmRepository.getFilmById(film_id);
-    }
-
-    async createFilm(film: CreateFilmDTO): Promise<FilmDTO> {
-        return this.filmRepository.createFilm(film);
-    }
-
-    async updateFilm(id: number, film: UpdateFilmDTO): Promise<FilmDTO> {
-        return this.filmRepository.updateFilm(id, film);
-    }
-
-    async deleteFilm(id: number): Promise<void> {
-        return this.filmRepository.deleteFilm(id);
-    }
-
-}
+export interface FilmService 
+    extends BaseService<FilmDTO, CreateFilmDTO, UpdateFilmDTO> {}
