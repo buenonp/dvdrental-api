3d2edd97dd7f558b0984f65ea24ea26e1555a1e3
Bueno Nepomuceno <buenonp@gmail.com>
2023-06-08 18:20:46 -0300

    film routes updated

--- src/modules/movies/film/api/routes/film.routes.ts
+++ src/modules/movies/film/api/routes/film.routes.ts
@@ -1,19 +1,17 @@
 import express from 'express';
 
+import { container } from 'tsyringe';
+
 import { FilmController } from '../controllers/FilmController';
-import { FilmService } from '../../business/services/FilmService';
-import { FilmRepository } from '../../database/repositories/FilmRepository';
 
 const router = express.Router();
 
-const filmRepository = new FilmRepository();
-const filmService = new FilmService(filmRepository);
-const filmController = new FilmController(filmService);
+const filmController: FilmController = container.resolve('FilmController');
 
-router.get('/', (req, res) => filmController.getFilms(req, res));
-router.get('/:id', (req, res) => filmController.getFilmById(req, res));
-router.post('/', (req, res) => filmController.createFilm(req, res));
-router.put('/:id', (req, res) => filmController.updateFilm(req, res));
-router.delete('/:id', (req, res) => filmController.deleteFilm(req, res));
+router.get('/', (req, res) => filmController.getItems(req, res));
+router.get('/:id', (req, res) => filmController.getItemById(req, res));
+router.post('/', (req, res) => filmController.createItem(req, res));
+router.put('/:id', (req, res) => filmController.updateItem(req, res));
+router.delete('/:id', (req, res) => filmController.deleteItem(req, res));
 
 export default router;
